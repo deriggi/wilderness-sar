e9a77488eae39ce938cbd040890de3cbcd82578d
deriggi <john.deriggi@gmail.com>
2013-06-27 21:18:22 +0100

    considering elevation on ground walker

--- src/main/java/strategy/updater/SlowOnSteepDirectionUpdater.java
+++ src/main/java/strategy/updater/SlowOnSteepDirectionUpdater.java
@@ -20,7 +20,7 @@ import strategy.updater.message.UpdaterMessage;
 public class SlowOnSteepDirectionUpdater extends SkelatalDirectionUpdater {
 
     private static final Logger log=  Logger.getLogger(SlowOnSteepDirectionUpdater.class.getName());
-    
+    private Float lastHeight= null;
     @Override
     public void updateDirection(double[] dxDy, VectorAgent ownerAgent) {
         Raster2D raster = RasterLoader.get(RasterConfig.BIG).getData();
@@ -38,7 +38,23 @@ public class SlowOnSteepDirectionUpdater extends SkelatalDirectionUpdater {
         if(slope < 0.30f && howSlow < 0){
             dxDy = VectorUtils.multiplyDonTouch(dxDy, new Float(ownerAgent.getSpeed()/howSlow));
         }
-        
+        float thisHeight = raster.getCell(loc[0],loc[1]);
+        if(lastHeight != null ){
+            float diff = Math.abs(lastHeight - thisHeight);
+            if(diff > 0 && diff/lastHeight > 0.002f ){
+                log.log(Level.INFO, "last height was {0} with a percent diff of {1} ", new Object[]{new Float(lastHeight),new Float(diff/lastHeight) });
+                log.log(Level.INFO, "slowing down {0}, {1}", new Object[]{new Float(dxDy[0]),new Float(dxDy[1]) });
+                dxDy = VectorUtils.add(dxDy, VectorUtils.multiplyDonTouch(dxDy, -0.50f));
+                log.log(Level.INFO, "slowing result: {0}, {1}", new Object[]{new Float(dxDy[0]),new Float(dxDy[1]) });
+            }else if (diff > 0){
+                // speed up a little
+                log.log(Level.INFO, "speeding: {0}, {1}", new Object[]{new Float(dxDy[0]),new Float(dxDy[1]) });
+                dxDy = VectorUtils.add(dxDy, VectorUtils.multiplyDonTouch(dxDy, 0.50f));
+                log.log(Level.INFO, "speeding result: {0}, {1}", new Object[]{new Float(dxDy[0]),new Float(dxDy[1]) });
+            }
+            
+        }
+        lastHeight = thisHeight;
     }
 
     @Override
